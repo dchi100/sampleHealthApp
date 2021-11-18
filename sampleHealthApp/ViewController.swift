b517c01c767dbcae91788e4c4d69b1088e3cb506
dchi100 <ydchi@clemson.edu>
2021-11-18 14:54:44 -0500

    Commit with working step query

--- sampleHealthApp/ViewController.swift
+++ sampleHealthApp/ViewController.swift
@@ -6,12 +6,69 @@
 //
 
 import UIKit
+import HealthKit
+
+
 
 class ViewController: UIViewController {
+    let healthStore = HKHealthStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func authoriseHealthKitAccess(_ sender: Any) {
+        let healthKitTypes: Set = [
+            // access step count
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        ]
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (_, _) in
+            print("authrised???")
+        }
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
+            if let e = error {
+                print("oops something went wrong during authorisation \(e.localizedDescription)")
+            } else {
+                print("User has completed the authorization flow")
+            }
+        }
+    }
+
+    func getTodaysSteps(completion: @escaping (Double) -> Void) {
+        
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            var resultCount = 0.0
+            guard let result = result else {
+                print("Failed to fetch steps rate")
+                completion(resultCount)
+                return
+            }
+            if let sum = result.sumQuantity() {
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            }
+            
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+    @IBOutlet weak var totalSteps: UILabel!
+    @IBAction func getTotalSteps(_ sender: Any) {
+        getTodaysSteps { (result) in
+            print("\(result)")
+            DispatchQueue.main.async {
+                self.totalSteps.text = "\(result)"
+            }
+        }
     }
 
 
